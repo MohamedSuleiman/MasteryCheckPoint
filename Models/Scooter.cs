25f9fe0968d966c8cfe7820ae093b4cfe12fd85c
Mohammed Suleiman <mohammed.suleiman@brightstraining.com>
2026-09-03 09:03:57 +0200

    Added class Status and created enum to represent status

---
+++ Models/Scooter.cs
@@ -0,0 +1,13 @@
+﻿namespace Models
+{
+    public class Scooter
+    {
+        public int Id { get; set; }
+        public string Brand { get; set; } = string.Empty;
+
+        public float BatteryCapacity { get; set; }
+
+        public StatusType Status { get; set;  }
+
+    }
+}
