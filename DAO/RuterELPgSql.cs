ce26753359c164de3fabe03c6e8ee6aa552cf66f
Mohammed Suleiman <mohammed.suleiman@brightstraining.com>
2026-09-03 10:34:29 +0200

    Added methods to start and end trips in Trip class

--- DAO/RuterELPgSql.cs
+++ DAO/RuterELPgSql.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +7,29 @@ namespace DAO
 {
     public class RuterELPgSql : RuterELDao
     {
+        public List<Trip> FindAllTripsForUserSortedByStart(int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Trip> FindAllTripsNotFinished()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Scooter> FindAvailableScootersOver20()
+        {
+            throw new NotImplementedException();
+        }
+
+        public double FindAveragePricePerKmFinishedTrips()
+        {
+            throw new NotImplementedException();
+        }
+
+        public AppUser? FindUserWithMostTrips()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- Models/Trip.cs
+++ Models/Trip.cs
@@ -4,6 +4,10 @@ using System.Text;
 
 namespace Models
 {
+    //Kunne hatt en konstruktør som tvinger den til
+    //å passe inn både User og Scooter
+    // men følte ikke behovet for de i en så liten
+    //oppgave
     public class Trip
     {
         public int Id { get; set; }
@@ -22,5 +26,25 @@ namespace Models
         public int ScooterId { get; set; }
         
         public Scooter? Scooter { get; set; }
+
+
+        public void StartTrip()
+        {
+            if (Scooter != null)
+            {
+                Scooter.Status = StatusType.InUse;
+            }
+            StartTime = DateTime.Now;
+        }
+
+        public void EndTrip()
+        {
+            if (Scooter != null)
+            {
+                Scooter.Status = StatusType.Available;
+            }
+            EndTime = DateTime.Now;
+
+        }
     }
 }
