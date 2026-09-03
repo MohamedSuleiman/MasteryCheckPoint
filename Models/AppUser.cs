5fa85c7543c306a99434c02adb831d36299c41ba
Mohammed Suleiman <mohammed.suleiman@brightstraining.com>
2026-09-03 09:22:13 +0200

    Added Scooter and Trip classes

---
+++ Models/AppUser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models
+{
+    public class AppUser
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+
+        //Kunne ha brukt data annotations for max og min length,
+        //men ikke nødevendigvis alle som bruker 8nr
+        public int PhoneNumber { get; set; }
+    }
+}
---
+++ Models/Trip.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models
+{
+    public class Trip
+    {
+        public int Id { get; set; }
+        public DateTime StartTime { get; set; }
+
+        public DateTime? EndTime { get; set; } = null;
+
+        public double Km { get; set; }
+
+        public double Cost { get; set; }
+
+        public int AppUserId { get; set; }
+
+        public AppUser? AppUser { get; set; }
+    }
+}
