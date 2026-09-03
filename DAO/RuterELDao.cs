29cb71f85ec97776f6cbba7a84ec63a771e64a19
Mohammed Suleiman <mohammed.suleiman@brightstraining.com>
2026-09-03 10:15:59 +0200

    Added Interface for central db agnostic operations

---
+++ DAO/RuterELDao.cs
@@ -0,0 +1,22 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAO
+{
+    public interface RuterELDao
+    {
+        public List<Scooter> FindAvailableScootersOver20();
+        public List<Trip> FindAllTripsForUserSortedByStart(int userId);
+
+        public List<Trip> FindAllTripsNotFinished();
+
+        // Nullable vil kun være relvant
+        //når vi ikke har noen registerte turer eller
+        //bruker, så kunne ha i teorien fjernet det
+        public AppUser? FindUserWithMostTrips();
+
+        public double FindAveragePricePerKmFinishedTrips();
+    }
+}
